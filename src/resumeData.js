5de0d81d03084d189e870c7fdcfdaaa4045347d6
anwarkhan0 <anwarsyed01234@gmail.com>
2022-08-27 15:35:36 +0500

    make it better

---
+++ src/resumeData.js
@@ -0,0 +1,111 @@
+let resumeData = {
+    "imagebaseurl":"https://media-exp1.licdn.com/dms/image/D4D35AQE3c3zL_D2CcA/profile-framedphoto-shrink_200_200/0/1656709246175?e=1662195600&v=beta&t=bFEOmxisJDCbunltwoLc7KNzZ1Wo0stDVTdft8fojjs",
+    "name": "S.Anwar Khan",
+    "role": "Web Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of software development and like to learn about new technologies and play sports in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/anwar-khan-4017831a4/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/anwarkhan0",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I completed my graduation at Uninversity of Swat. I am a self taught Full Stack Web Developer, recently joined a company as a junior software engineer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Pakistan",
+    "website":"https://github.com/anwarkhan0",
+    "education":[
+      {
+        "UniversityName":"University Of Swat",
+        "specialization":"BS Computer Science",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2020"
+      },
+      // {
+      //   "UniversityName":"Some University",
+      //   "specialization":"Some specialization",
+      //   "MonthOfPassing":"Jan",
+      //   "YearOfPassing":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "work":[
+      {
+        "CompanyName":"Skysoftconnections",
+        "specialization":"Junior Software Engineer",
+        // "MonthOfLeaving":"",
+        // "YearOfLeaving":"",
+        // "Achievements":"Some Achievements"
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname": "Nodejs"
+      },
+      {
+        "skillname": "Javascript"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // },
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // }
+    ]
+  }
+  
+  export default resumeData
